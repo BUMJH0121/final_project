11b0673347e456a229342e5f69f06107bfbbf04a
BUMJH0121 <bumjh0121@gmail.com>
2021-05-26 11:20:43 +0900

    modifing...

--- base_changed/skin_assistant/app.py
+++ base_changed/skin_assistant/app.py
@@ -12,7 +12,7 @@ from flask import render_template
 from flask import session
 from flask import url_for
 from authlib.integrations.flask_client import OAuth
-#from six.moves.urllib.parse import urlencode
+from six.moves.urllib.parse import urlencode
 import sys
 #from DB_handler import DBModule
 # import database
@@ -42,7 +42,7 @@ def rest():
 
 @app.route("/login")
 def login():
-    return auth0.authorize_redirect(redirect_uri='http://15.237.112.4:5000/callback')
+    return auth0.authorize_redirect(redirect_uri='http://127.0.0.1:5000/callback')
 
 @app.route("/logout")
 def logout():
@@ -61,12 +61,13 @@ def callback_handling():
     userinfo = resp.json()
     # Store the user information in flask session.
     session['jwt_payload'] = userinfo
+    print(session['jwt_payload'])
     session['profile'] = {
         'user_id': userinfo['sub'],
         'name': userinfo['name'],
         'picture': userinfo['picture']
     }
-    return redirect('/dashboard')
+    return redirect('/home')
 
 
 def requires_auth(f):
@@ -74,7 +75,7 @@ def requires_auth(f):
     def decorated(*args, **kwargs):
         if 'profile' not in session:
         # Redirect to Login page here
-            return redirect('/')
+            return redirect('/login')
         return f(*args, **kwargs)
     return decorated
 
@@ -103,21 +104,26 @@ def signin_done():
 
 @app.route("/home")
 def home():
+    user_name = session.get('profile', None)
+    print(user_name)
     #graph = f"/graph_img.jpg"
     #return render_template("Home.html", graph=graph)
-    return render_template("Home.html")
+    return render_template("Home.html", usern = user_name)
 
 @app.route("/home/graph")
 def graph():
-    return render_template("Graph.html")
+    user_name = session.get('profile', None)
+    return render_template("Graph.html", usern = user_name)
 
 @app.route("/home/photo")
 def photo():
-    return render_template("Photo.html")
+    user_name = session.get('profile', None)
+    return render_template("Photo.html", usern = user_name)
     
 @app.route("/home/product")
 def product():
-    return render_template("Product.html")
+    user_name = session.get('profile', None)
+    return render_template("Product.html", usern = user_name)
 
 
 
