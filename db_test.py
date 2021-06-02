e834b2a1e825cca4caaf2ebf50a711248a75f128
BUMJH0121 <bumjh0121@gmail.com>
2021-06-02 16:19:40 +0900

    final

--- db_test.py
+++ db_test.py
@@ -1,8 +1,11 @@
-import json
-import datetime
-d =datetime.datetime.now().strftime("%Y-%m-%d")
-temp = {}
-temp[d] = 1
-t = json.dumps(temp, ensure_ascii=False)
+import pymysql
 
-print(t, type(t))
+test_db = pymysql.connect(user='root', passwd='team09', host='35.180.122.212', db='mydb', charset='utf8')
+cursor = test_db.cursor(pymysql.cursors.DictCursor)
+sql = "select distinct img_url1, date from user_face where machine_no = (select machine_no from user_info where user_id = '{}') limit 30".format("google-oauth2|107217289318141912711")
+cursor.execute(sql)
+result = cursor.fetchall()
+
+
+test_db.close()
+print(result)
